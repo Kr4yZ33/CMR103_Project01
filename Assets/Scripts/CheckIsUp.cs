6dc97363568410e48b4be99bbf01a7acf98cdaab
jpernesz <1020894@student.sae.edu.au>
2020-10-15 20:15:29 +1100

    Initial Commit

---
+++ Assets/Scripts/CheckIsUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIsUp : MonoBehaviour
+{
+    public float tolerance = 0.9f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsUpsideDown())
+        {
+            Debug.Log("Is Upside Down");
+        } else
+        {
+            Debug.Log("Is Right Side Up");
+        }
+
+        float valueOfDotProduct = Vector3.Dot(transform.up, Vector3.up);
+        Debug.Log("The Dot Product is " + valueOfDotProduct);
+    }
+
+    public bool IsUpsideDown()
+    {
+        return (Vector3.Dot(transform.up, Vector3.up) < -tolerance);
+    }
+}
---
+++ Assets/Scripts/JPHandScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JPHandScript : MonoBehaviour
+{
+    public JPColourChange theCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var hand = GetComponent<OVRHand>();
+        bool isIndexFingerPinching = hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+
+        if (isIndexFingerPinching)
+        {
+            //is pinching., do something
+            theCube.SetGreen();
+
+        }
+        else
+        {
+            //not pinching
+            theCube.SetRed();
+        }
+
+        float ringFingerPinchStrength = hand.GetFingerPinchStrength(OVRHand.HandFinger.Ring);
+    }
+}
